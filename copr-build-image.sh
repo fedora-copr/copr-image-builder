7b9add110604d32cb4ab474f8c67005d1d13c686
Jakub Kadlcik <frostyx@email.cz>
2025-02-28 13:49:07 +0100

    Add a helper script for building images

---
+++ copr-build-image.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ -z "${IMAGE_TYPE}" ]; then
+    echo "Set IMAGE_TYPE to qcow2 or ami"
+    exit 1
+fi
+
+# dnf -y copr enable @osbuild/image-builder
+# At least now, we have to fully update the system because otherwise
+# image-buidler fails with some cryptic DNF error
+dnf -y update
+
+dnf -y copr enable @osbuild/image-builder
+dnf -y install wget image-builder
+wget https://raw.githubusercontent.com/fedora-copr/copr-image-builder/refs/heads/main/config.toml
+
+# We have a massive RAM but very limited disk space
+image-builder build "$IMAGE_TYPE" \
+    --blueprint ./config.toml \
+    --output-dir /tmp \
+    --cache /tmp \
+    --data-dir /tmp
+
+echo "Generated image:"
+find /tmp/ -name disk.qcow2
+find /tmp/ -name image.raw
