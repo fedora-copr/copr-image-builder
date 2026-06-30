824f51586d9804ca679306c315875a63a1c99fb5
Jakub Kadlcik <frostyx@email.cz>
2026-06-30 16:08:24 +0200

    Don't use sudo podman aka rootless builds
    There is an experimental support for rootless builds
    https://osbuild.org/docs/bootc/#rootless

--- copr-build-image-bootc.sh
+++ copr-build-image-bootc.sh
@@ -18,31 +18,32 @@ fi
 if [ "$BUILD_OCI" == true ]; then
     # The LINUX_IMMUTABLE is needed for doing chattr when building the image.
     # We use it for Internal Copr builders
-    sudo podman build \
+    podman build \
         --cap-add LINUX_IMMUTABLE \
         --network host \
         -t $IMAGE \
         $CONTEXT \
         || exit 1
 else
-    sudo podman pull $IMAGE || exit 1
+    podman pull $IMAGE || exit 1
 fi
 
 
 if [ "$BUILD_BOOTC" == true ]; then
     mkdir -p output
-    sudo podman run \
+    podman run \
          --rm \
          -it \
          --privileged \
          --pull=newer \
          --security-opt label=type:unconfined_t \
          -v ./output:/output \
-         -v /var/lib/containers/storage:/var/lib/containers/storage \
+         -v ~/.local/share/containers/storage:/var/lib/containers/storage \
          quay.io/centos-bootc/bootc-image-builder:latest \
          --type "$IMAGE_TYPE" \
          --rootfs xfs \
          --use-librepo=True \
+         --in-vm \
          $IMAGE \
          || exit 1
 
